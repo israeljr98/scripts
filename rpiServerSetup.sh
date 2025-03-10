d7925c33bac0e78dd40dc62cc25acc3d0b8798b6
Israel Rodriguez <israeljr98@berkeley.edu>
2025-03-10 00:53:23 -0700

    Added bits for code-server installation

--- rpiServerSetup.sh
+++ rpiServerSetup.sh
@@ -67,3 +67,10 @@ docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 echo "Portainer installed. Access here: https://$(hostname).local:9443"
+
+
+
+# Code Server Install
+
+curl -fsSL https://code-server.dev/install.sh | sh
+sudo systemctl enable --now code-server@$USER
